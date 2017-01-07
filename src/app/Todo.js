e1bc5a8ebe9ca4e14c856a44370d1d75dde9acef
tpyoung <tylerparkeyoung@gmail.com>
2017-01-06 21:23:21 -0800

    Creates basic react todo list

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+
 export class Todo extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			todos: [],
+			newTodo: ''
+		}
+	}
+
+	handleChange(e){
+		const {value} = e.target;
+		this.setState({newTodo: value});
+	}
+	handleClick(e){
+		e.preventDefault();
+		const todos = [...this.state.todos, this.state.newTodo];
+		this.setState({todos, newTodo: ''});
+	}
   render() {
     return (
       <div>
-      	HEEEEEELLLLOOOOO WOOOOOORRRRLLLLLLDDDD
+      	<form>
+      		<input onChange={this.handleChange.bind(this)} value={this.state.newTodo} type='text' placeholder='new todo item' />
+      		<button onClick={this.handleClick.bind(this)}>Add Item</button>
+      	</form>
+      	<ul>	
+					{this.state.todos.map(todo => (
+							<li>{todo}</li>
+						))
+					}
+      	</ul>
       </div>
     );
   }
